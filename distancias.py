95ff30ab379f3464fcc7a1b9f32a2d5d4a126533
Alina-02 <alinamontma@gmail.com>
2024-10-20 17:14:02 +0200

    levenshtein con menos espacio y threshold

--- distancias.py
+++ distancias.py
@@ -17,6 +17,32 @@ def levenshtein_matriz(x, y, threshold=None):
             )
     return D[lenX, lenY]
 
+def new_levenshtein_matriz(x, y, threshold=None):
+
+    toomuch = False #si se pasa del threshold
+    lenX, lenY = len(x), len(y) #longitud de las cadenas
+    current_row = [None] * (1 + lenX) #la fila actual (longitud de la primera palabra + 1)
+    previous_row = [None] * (1 + lenX) #la fila previa (longitud de la primera palabra + 1)
+    current_row[0] = 0 
+
+    for i in range(1, lenX + 1): #desde el principio de la primera palabra hasta su final
+        current_row[i] = current_row[i - 1] + 1 #la columna actual es es coste de la columna anterior + 1
+    for j in range(1, lenY + 1): #recorre la segunda palabra
+        if toomuch: break
+        previous_row, current_row = current_row, previous_row #la fila actual se guarda en previous y se modifica la actual (usando la previous)
+        current_row[0] = previous_row[0] + 1 #inicializa el primer elemento de la nueva fila
+        for i in range(1, lenX + 1): #añade los costes de la fila
+            current_row[i] = min( #elige el mínimo entre
+                current_row[i - 1] + 1, #lado izquierdo
+                previous_row[i] + 1, #abajo
+                previous_row[i - 1] + (x[i - 1] != y[j - 1]), #diagonal
+            )
+            if current_row[i] > threshold: 
+                current_row[lenX] = None
+                threshold = True
+                break
+    return current_row[lenX]
+
 def levenshtein_edicion(x, y, threshold=None):
     # a partir de la versión levenshtein_matriz
     return 0,[] # COMPLETAR Y REEMPLAZAR ESTA PARTE
